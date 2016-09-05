54863d2a914574005e384c94bff33737c3dcc6f0
VolhaShyrko <olgashyrko@gmail.com>
2016-09-05 13:23:37 +0300

    first commit

---
+++ Reflection1/ListGenerator.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Reflection1
+{
+    public class ListGenerator
+    {
+        public IList ListInstance(Type t)
+        {
+            Type listType = typeof(List<>);
+            Type constructedListType = listType.MakeGenericType(t);
+            var instance = (IList)Activator.CreateInstance(constructedListType);
+   
+            return instance;
+        }
+    }
+}
---
+++ Reflection1/Program.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Reflection1
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Reflection;
+
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            ListGenerator generator = new ListGenerator();
+            IList instance = generator.ListInstance(typeof(string));
+            instance.Add("abc");
+            instance.Add("");
+        }
+    }
+}
